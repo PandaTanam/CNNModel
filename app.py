e50c8553112cd68e2fa9e679a8ec87e4c1f62739
szyxxx <axeldavid1521@gmail.com>
2024-11-28 14:31:33 +0700

    add: user_id and treatment suggestion

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import io
+import google.generativeai as genai
 
 app = Flask(__name__)
 
@@ -19,6 +20,8 @@ class_names = {
     # 'apple': ['Apple_Disease_1', 'Apple_Disease_2', 'Apple_Disease_3']  
 }
 
+scanned_data = []
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -29,6 +32,7 @@ def predict():
         return jsonify({'error': 'No file selected'}), 400
 
     plant_type = request.form.get('plant_type')
+    user_id = request.form.get('user_id')
     if plant_type not in class_names:
         return jsonify({'error': 'Invalid plant type'}), 400
 
@@ -46,10 +50,45 @@ def predict():
     predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions)
 
-    return jsonify({
-        'class': class_names[plant_type][predicted_class],
-        'probability': float(predictions[0][predicted_class])
-    })
+    disease_name = class_names[plant_type][predicted_class]
+
+    result = {
+        'user_id': user_id,
+        'plant_type': plant_type,
+        'disease': disease_name,
+        'probability': float(predictions[0][predicted_class]),
+        'treatment': None
+    }
+    scanned_data.append(result)
+
+    return jsonify(result)
+
+@app.route('/treatment', methods=['POST'])
+def treatment():
+    genai.configure(api_key="AIzaSyCXrHQKYgn2VWxe3iGaxz7y55U9ogdJU3I")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+
+    disease = request.json.get('disease')
+    plant = request.json.get('plant')
+    user_id = request.json.get('user_id')
+
+    if not disease or not plant or not user_id:
+        return jsonify({'error': 'Plant, Disease, and User ID must be provided'}), 400
+
+    prompt = f"Langkah-langkah mengatasi/merawat {plant} yang terkena penyakit {disease} dengan penjelasan singkat dan tepat"
+
+    treatment_suggestion = model.generate_content(prompt)
+
+    for entry in scanned_data :
+        if entry['user_id'] == user_id :
+            entry['treatment'] = treatment_suggestion.text
+            break
+
+    return jsonify({'treatment': treatment_suggestion.text})
+
+@app.route('/scanned_data', methods=['GET'])
+def get_scanned_data():
+    return jsonify(scanned_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)

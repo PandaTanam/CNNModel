e8ff3cbb96947900542d635ae38898f8bdc8e7ea
szyxxx <axeldavid1521@gmail.com>
2024-12-02 10:40:54 +0700

    revert port

--- app.py
+++ app.py
@@ -148,4 +148,5 @@ def get_scanned_data():
     return jsonify(scanned_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+    port = int(os.environ.get("PORT", 8080)) 
+    app.run(host='0.0.0.0', port=port)

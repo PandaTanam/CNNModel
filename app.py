ff3f90815f4a8f7c260242ae08a570334e9f70e7
szyxxx <axeldavid1521@gmail.com>
2024-12-04 03:19:10 +0700

    yayayaya

--- app.py
+++ app.py
@@ -203,6 +203,3 @@ async def delete_prediction(user_id: str):
     except Exception as e:
         logging.error(f"Error deleting predictions: {str(e)}")
         raise HTTPException(status_code=500, detail=f'Error deleting predictions: {str(e)}')
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080, proxy_headers=True)

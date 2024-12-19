415e2c32112acde3f2f67863db9ce08a588ba048
szyxxx <axeldavid1521@gmail.com>
2024-12-19 13:10:00 +0700

    edit: fix bug

--- main.py
+++ main.py
@@ -200,6 +200,6 @@ async def get_news():
 
     return {"news": results} 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8080)
